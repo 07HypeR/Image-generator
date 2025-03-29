84e371bc67e0d16050144796267dbb70d65f9f29
Abhik Baidya <abhikbaidya7@gmail.com>
2025-03-29 20:38:14 +0530

    Bottom tabs & Screens added

--- App.tsx
+++ App.tsx
@@ -1,20 +1,53 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import Icon from 'react-native-vector-icons/AntDesign';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeScreen from './src/screens/HomeScreen';
+import DiscoverScreen from './src/screens/DiscoverScreen';
+import LikeScreen from './src/screens/LikeScreen';
+import {colors} from './src/theme';
+import Feather from 'react-native-vector-icons/Feather';
 
-const App = () => {
+const Tab = createBottomTabNavigator();
+
+export default function App() {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Icon name="API" size={30} color={'red'} />
-      <Text style={styles.text}>App</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: {backgroundColor: colors.primary},
+          tabBarActiveTintColor: colors.activeTabColor,
+          tabBarInactiveTintColor: colors.inactiveTabColor,
+          tabBarShowLabel: false,
+        }}>
+        <Tab.Screen
+          name="HOME_SCREEN"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({color, size, focused}) => (
+              <Feather name={'home'} color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="DISCOVER_SCREEN"
+          component={DiscoverScreen}
+          options={{
+            tabBarIcon: ({color, size, focused}) => (
+              <Feather name={'globe'} color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="LIKE_SCREEN"
+          component={LikeScreen}
+          options={{
+            tabBarIcon: ({color, size, focused}) => (
+              <Feather name={'heart'} color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-};
-
-export default App;
-
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 99,
-  },
-});
+}
---
+++ src/theme/index.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  primary: '#1E1E1E',
+  activeTabColor: '#FFFFFF',
+  inactiveTabColor: '#9DB2CE',
+};
+export const fontSize = {};
+export const theme = {
+  colors,
+  fontSize,
+};
