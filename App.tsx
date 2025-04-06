7e4d0bc17d85a541aa8a2481bc52a8942f90981b
Abhik Baidya <abhikbaidya7@gmail.com>
2025-04-06 20:17:29 +0530

    Liked image provider added

--- App.tsx
+++ App.tsx
@@ -6,48 +6,51 @@ import DiscoverScreen from './src/screens/DiscoverScreen';
 import LikeScreen from './src/screens/LikeScreen';
 import {colors} from './src/theme';
 import Feather from 'react-native-vector-icons/Feather';
+import {LikeImageProvider} from './src/context/LikeImageContext';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          headerShown: false,
-          tabBarStyle: {backgroundColor: colors.primary},
-          tabBarActiveTintColor: colors.activeTabColor,
-          tabBarInactiveTintColor: colors.inactiveTabColor,
-          tabBarShowLabel: false,
-        }}>
-        <Tab.Screen
-          name="HOME_SCREEN"
-          component={HomeScreen}
-          options={{
-            tabBarIcon: ({color, size, focused}) => (
-              <Feather name={'home'} color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="DISCOVER_SCREEN"
-          component={DiscoverScreen}
-          options={{
-            tabBarIcon: ({color, size, focused}) => (
-              <Feather name={'globe'} color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="LIKE_SCREEN"
-          component={LikeScreen}
-          options={{
-            tabBarIcon: ({color, size, focused}) => (
-              <Feather name={'heart'} color={color} size={size} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <LikeImageProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+            tabBarStyle: {backgroundColor: colors.primary},
+            tabBarActiveTintColor: colors.activeTabColor,
+            tabBarInactiveTintColor: colors.inactiveTabColor,
+            tabBarShowLabel: false,
+          }}>
+          <Tab.Screen
+            name="HOME_SCREEN"
+            component={HomeScreen}
+            options={{
+              tabBarIcon: ({color, size, focused}) => (
+                <Feather name={'home'} color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="DISCOVER_SCREEN"
+            component={DiscoverScreen}
+            options={{
+              tabBarIcon: ({color, size, focused}) => (
+                <Feather name={'globe'} color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="LIKE_SCREEN"
+            component={LikeScreen}
+            options={{
+              tabBarIcon: ({color, size, focused}) => (
+                <Feather name={'heart'} color={color} size={size} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </LikeImageProvider>
   );
 }
---
+++ src/context/LikeImageContext.js
@@ -0,0 +1,48 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {createContext, useEffect, useState} from 'react';
+import {ToastAndroid} from 'react-native';
+
+const LikeImagesContext = createContext(null);
+
+export const LikeImageProvider = ({children}) => {
+  const [likedImages, setLikedImages] = useState([]);
+
+  useEffect(() => {
+    loadLikedImages();
+  }, []);
+
+  const loadLikedImages = async () => {
+    let images = await AsyncStorage.getItem('likedImages');
+    images = images ? JSON.parse(images) : [];
+    setLikedImages(images);
+  };
+
+  const toogleLikeImage = async genImage => {
+    let likedImages = await AsyncStorage.getItem('likedImages');
+    likedImages = likedImages ? JSON.parse(likedImages) : [];
+
+    const imageExist = likedImages.some(item => item._id === genImage._id);
+
+    if (imageExist) {
+      likedImages = likedImages.filter(item => item._id !== genImage._id);
+      ToastAndroid.show('Liked image removed successfully', ToastAndroid.SHORT);
+    } else {
+      likedImages.push(genImage);
+      ToastAndroid.show('Image Liked successfully', ToastAndroid.SHORT);
+    }
+
+    setLikedImages(likedImages);
+    await AsyncStorage.setItem('likedImages', JSON.stringify(likedImages));
+  };
+
+  const value = {
+    likedImages,
+    toogleLikeImage,
+  };
+
+  return (
+    <LikeImagesContext.Provider value={value}>
+      {children}
+    </LikeImagesContext.Provider>
+  );
+};
