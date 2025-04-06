746e6cb5dad1fdea3b3e36f618f4d0b1c97e2ae2
Abhik Baidya <abhikbaidya7@gmail.com>
2025-04-06 21:53:07 +0530

    ENV file ignored

--- App.tsx
+++ App.tsx
@@ -7,50 +7,58 @@ import LikeScreen from './src/screens/LikeScreen';
 import {colors} from './src/theme';
 import Feather from 'react-native-vector-icons/Feather';
 import {LikeImageProvider} from './src/context/LikeImageContext';
+import {BannerAd, BannerAdSize, TestIds} from 'react-native-google-mobile-ads';
 
 const Tab = createBottomTabNavigator();
+const adUnitId = __DEV__ ? TestIds.ADAPTIVE_BANNER : process.env.GOOGLE_UID;
 
 export default function App() {
   return (
-    <LikeImageProvider>
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={{
-            headerShown: false,
-            tabBarStyle: {backgroundColor: colors.primary},
-            tabBarActiveTintColor: colors.activeTabColor,
-            tabBarInactiveTintColor: colors.inactiveTabColor,
-            tabBarShowLabel: false,
-          }}>
-          <Tab.Screen
-            name="HOME_SCREEN"
-            component={HomeScreen}
-            options={{
-              tabBarIcon: ({color, size, focused}) => (
-                <Feather name={'home'} color={color} size={size} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="DISCOVER_SCREEN"
-            component={DiscoverScreen}
-            options={{
-              tabBarIcon: ({color, size, focused}) => (
-                <Feather name={'globe'} color={color} size={size} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="LIKE_SCREEN"
-            component={LikeScreen}
-            options={{
-              tabBarIcon: ({color, size, focused}) => (
-                <Feather name={'heart'} color={color} size={size} />
-              ),
-            }}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </LikeImageProvider>
+    <>
+      <LikeImageProvider>
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={{
+              headerShown: false,
+              tabBarStyle: {backgroundColor: colors.primary},
+              tabBarActiveTintColor: colors.activeTabColor,
+              tabBarInactiveTintColor: colors.inactiveTabColor,
+              tabBarShowLabel: false,
+            }}>
+            <Tab.Screen
+              name="HOME_SCREEN"
+              component={HomeScreen}
+              options={{
+                tabBarIcon: ({color, size, focused}) => (
+                  <Feather name={'home'} color={color} size={size} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="DISCOVER_SCREEN"
+              component={DiscoverScreen}
+              options={{
+                tabBarIcon: ({color, size, focused}) => (
+                  <Feather name={'globe'} color={color} size={size} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="LIKE_SCREEN"
+              component={LikeScreen}
+              options={{
+                tabBarIcon: ({color, size, focused}) => (
+                  <Feather name={'heart'} color={color} size={size} />
+                ),
+              }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </LikeImageProvider>
+      <BannerAd
+        unitId={adUnitId}
+        size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+      />
+    </>
   );
 }
